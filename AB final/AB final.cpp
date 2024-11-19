e92db16e6b8965dfed1077cbc9d2eb2f788fb86e
LUCKYS2023 <156194893+LUCKYS2023@users.noreply.github.com>
2024-11-19 18:16:27 +0100

    Add files via upload

---
+++ AB final/AB final.cpp
@@ -0,0 +1,12 @@
+﻿// AB final.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "AB final.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
